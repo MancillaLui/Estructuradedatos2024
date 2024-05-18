c968a15b60f7754174bdee1dd80f024b17791af9
Octanexd96 <119269251+Octanexd96@users.noreply.github.com>
2024-05-17 22:07:19 -0500

    .

---
+++ Tarea Listas/preunta1/preunta1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de números separados por espacios:");
+        string input = Console.ReadLine();
+
+        HashSet<int> numeros = new HashSet<int>(input.Split(' ').Select(int.Parse));
+
+        HashSet<int> primos = ObtenerNumerosPrimos(numeros);
+
+        Console.WriteLine("Números primos: " + string.Join(", ", primos));
+    }
+
+    public static HashSet<int> ObtenerNumerosPrimos(HashSet<int> numeros)
+    {
+        HashSet<int> primos = new HashSet<int>();
+
+        foreach (int numero in numeros)
+        {
+            if (EsPrimo(numero))
+            {
+                primos.Add(numero);
+            }
+        }
+
+        return primos;
+    }
+
+    private static bool EsPrimo(int numero)
+    {
+        if (numero <= 1) return false;
+        if (numero == 2) return true;
+        if (numero % 2 == 0) return false;
+
+        int limite = (int)Math.Sqrt(numero);
+        for (int i = 3; i <= limite; i += 2)
+        {
+            if (numero % i == 0) return false;
+        }
+
+        return true;
+    }
+}
---
+++ pregunta1examen/Program.cs
@@ -0,0 +1,16 @@
+﻿using system;
+
+class Program
+{
+
+    static void Main()
+    {
+        //Vamos a pedir el lado...
+
+        Console.Write("Ingrese la unidad de lados: ");
+        int lado = int.Parse(Console.Readline());
+
+        int dmenor = (int)Math
+
+    }
+}
