4c077d8716573b225d1c2745aae2d7f1d7c9cca6
Octanexd96 <119269251+Octanexd96@users.noreply.github.com>
2024-05-17 22:55:04 -0500

    .

---
+++ Tarea Listas/pregunta13/pregunta13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de números separados por espacios:");
+        string input = Console.ReadLine();
+        string[] numerosStr = input.Split(' ');
+        List<int> numeros = new List<int>();
+
+        foreach (string numStr in numerosStr)
+        {
+            int num;
+            if (int.TryParse(numStr, out num))
+            {
+                numeros.Add(num);
+            }
+        }
+        HashSet<int> numerosDuplicados = ObtenerNumerosDuplicados(numeros);
+        Console.WriteLine("Números duplicados:");
+        foreach (int num in numerosDuplicados)
+        {
+            Console.Write(num + " ");
+        }
+    }
+    public static HashSet<int> ObtenerNumerosDuplicados(List<int> numeros)
+    {
+        HashSet<int> numerosVistos = new HashSet<int>();
+        HashSet<int> numerosDuplicados = new HashSet<int>();
+
+        foreach (int num in numeros)
+        {
+            if (!numerosVistos.Add(num))
+            {
+                numerosDuplicados.Add(num);
+            }
+        }
+
+        return numerosDuplicados;
+    }
+}
---
+++ Tarea Listas/pregunta14/pregunta14/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de números separados por espacios:");
+        string input = Console.ReadLine();
+        string[] numerosStr = input.Split(' ');
+        List<int> numeros = new List<int>();
+        foreach (string numStr in numerosStr)
+        {
+            int num;
+            if (int.TryParse(numStr, out num))
+            {
+                numeros.Add(num);
+            }
+        }
+        HashSet<int> numerosNoDuplicados = ObtenerNumerosNoDuplicados(numeros);
+        Console.WriteLine("Números que no están duplicados:");
+        foreach (int num in numerosNoDuplicados)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    public static HashSet<int> ObtenerNumerosNoDuplicados(List<int> numeros)
+    {
+        Dictionary<int, int> frecuencia = new Dictionary<int, int>();
+        foreach (int num in numeros)
+        {
+            if (frecuencia.ContainsKey(num))
+            {
+                frecuencia[num]++;
+            }
+            else
+            {
+                frecuencia[num] = 1;
+            }
+        }
+        HashSet<int> numerosNoDuplicados = new HashSet<int>();
+        foreach (var kvp in frecuencia)
+        {
+            if (kvp.Value == 1)
+            {
+                numerosNoDuplicados.Add(kvp.Key);
+            }
+        }
+
+        return numerosNoDuplicados;
+    }
+}
