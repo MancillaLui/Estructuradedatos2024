1b8610b58109a2032b85d98e8cbd24a027d9a46a
Octanexd96 <119269251+Octanexd96@users.noreply.github.com>
2024-05-17 22:59:59 -0500

    .

--- Tarea Listas/pregunta15/pregunta15/Program.cs
+++ Tarea Listas/pregunta15/pregunta15/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de números separados por espacios:");
+        string input = Console.ReadLine();
+        string[] numerosStr = input.Split(' ');
+        List<int> numeros = new List<int>();
+        foreach (string numStr in numerosStr)
+        {
+            int num;
+            if (int.TryParse(numStr, out num))
+            {
+                numeros.Add(num);
+            }
+        }
+        HashSet<int> primos = ObtenerNumerosPrimos(numeros);
+        Console.WriteLine("Números primos ordenados de menor a mayor:");
+        foreach (int primo in primos)
+        {
+            Console.Write(primo + " ");
+        }
+    }
+    public static HashSet<int> ObtenerNumerosPrimos(List<int> numeros)
+    {
+        HashSet<int> primos = new HashSet<int>();
+
+        foreach (int num in numeros)
+        {
+            if (EsPrimo(num))
+            {
+                primos.Add(num);
+            }
+        }
+        return primos;
+    }
+    public static bool EsPrimo(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Tarea Listas/pregunta16/pregunta16/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de palabras separadas por espacios:");
+        string input = Console.ReadLine();
+        string[] palabras = input.Split(' ');
+        HashSet<string> palindromasOrdenadas = ObtenerPalindromasOrdenadas(palabras);
+        Console.WriteLine("Palabras palíndromas ordenadas de menor a mayor:");
+        foreach (string palindroma in palindromasOrdenadas)
+        {
+            Console.WriteLine(palindroma);
+        }
+    }
+
+    public static HashSet<string> ObtenerPalindromasOrdenadas(string[] palabras)
+    {
+        HashSet<string> palindromas = new HashSet<string>();
+        foreach (string palabra in palabras)
+        {
+            if (EsPalindromo(palabra))
+            {
+                palindromas.Add(palabra);
+            }
+        }
+        List<string> palindromasOrdenadas = palindromas.ToList();
+        palindromasOrdenadas.Sort();
+
+        return new HashSet<string>(palindromasOrdenadas);
+    }
+    public static bool EsPalindromo(string palabra)
+    {
+        int izquierda = 0;
+        int derecha = palabra.Length - 1;
+
+        while (izquierda < derecha)
+        {
+            if (palabra[izquierda] != palabra[derecha])
+            {
+                return false;
+            }
+            izquierda++;
+            derecha--;
+        }
+        return true;
+    }
+}
