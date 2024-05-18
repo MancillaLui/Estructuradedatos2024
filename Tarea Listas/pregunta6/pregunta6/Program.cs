a61a7594d72d91a3b452bcb960e381ef00f3f747
Octanexd96 <119269251+Octanexd96@users.noreply.github.com>
2024-05-17 22:21:24 -0500

    .

---
+++ Tarea Listas/pregunta6/pregunta6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese el primer conjunto de números separados por espacios:");
+        string input1 = Console.ReadLine();
+        HashSet<int> conjunto1 = new HashSet<int>(input1.Split(' ').Select(int.Parse));
+
+        Console.WriteLine("Ingrese el segundo conjunto de números separados por espacios:");
+        string input2 = Console.ReadLine();
+        HashSet<int> conjunto2 = new HashSet<int>(input2.Split(' ').Select(int.Parse));
+        HashSet<int> diferencia = ObtenerDiferencia(conjunto1, conjunto2);
+        Console.WriteLine("Números en el segundo conjunto pero no en el primero: " + string.Join(", ", diferencia));
+    }
+
+    public static HashSet<int> ObtenerDiferencia(HashSet<int> conjunto1, HashSet<int> conjunto2)
+    {
+        HashSet<int> diferencia = new HashSet<int>(conjunto2);
+
+        diferencia.ExceptWith(conjunto1);
+
+        return diferencia;
+    }
+}
