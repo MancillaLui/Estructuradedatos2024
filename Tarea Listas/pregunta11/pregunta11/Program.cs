81178251e85f762ab80a98e71e20445439960f0b
Octanexd96 <119269251+Octanexd96@users.noreply.github.com>
2024-05-17 22:47:37 -0500

    .

---
+++ Tarea Listas/pregunta11/pregunta11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de números separados por espacios:");
+        string input = Console.ReadLine();
+        string[] numerosStr = input.Split(' ');
+        List<int> numeros = new List<int>();
+        foreach (string numStr in numerosStr)
+        {
+            int num;
+            if (int.TryParse(numStr, out num))
+            {
+                numeros.Add(num);
+            }
+        }
+        HashSet<int> numerosOrdenados = ObtenerNumerosOrdenados(numeros);
+        Console.WriteLine("Números ordenados de menor a mayor:");
+        foreach (int num in numerosOrdenados)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    public static HashSet<int> ObtenerNumerosOrdenados(List<int> numeros)
+    {
+        numeros.Sort();
+        HashSet<int> numerosOrdenados = new HashSet<int>(numeros);
+        return numerosOrdenados;
+    }
+}
