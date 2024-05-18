0dd540a8b27ccaf1abf931b6245169afb1e72c25
Octanexd96 <119269251+Octanexd96@users.noreply.github.com>
2024-05-17 22:24:47 -0500

    .

--- Tarea Listas/pregunta9/pregunta9/Program.cs
+++ Tarea Listas/pregunta9/pregunta9/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un conjunto de palabras separadas por espacios:");
+        string input = Console.ReadLine();
+        string[] palabras = input.Split(' ');
+        Console.WriteLine("Ingrese la longitud deseada:");
+        int longitudDeseada;
+        while (!int.TryParse(Console.ReadLine(), out longitudDeseada) || longitudDeseada < 0)
+        {
+            Console.WriteLine("Por favor, ingrese una longitud válida (un número entero positivo):");
+        }
+        HashSet<string> palabrasConLongitud = ObtenerPalabrasConLongitud(palabras, longitudDeseada);
+        Console.WriteLine($"Palabras de longitud {longitudDeseada}: ");
+        foreach (var palabra in palabrasConLongitud)
+        {
+            Console.WriteLine(palabra);
+        }
+    }
+
+    public static HashSet<string> ObtenerPalabrasConLongitud(string[] palabras, int longitudDeseada)
+    {
+        HashSet<string> palabrasConLongitud = new HashSet<string>();
+        foreach (var palabra in palabras)
+        {
+            if (palabra.Length == longitudDeseada)
+            {
+                palabrasConLongitud.Add(palabra);
+            }
+        }
+
+        return palabrasConLongitud;
+    }
+}
